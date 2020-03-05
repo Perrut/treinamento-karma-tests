4c0cd992ffc9393427de9e4efe855f458c9c11de
Matheus Perrut <matheus.perrut@govbr.com.br>
2020-03-05 16:10:00 -0300

    Testes da tela principal do app

--- src/app/jogo/jogo/jogo.component.spec.ts
+++ src/app/jogo/jogo/jogo.component.spec.ts
@@ -1,17 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JogoComponent } from './jogo.component';
+import { ValidaRespostaDirective } from '../directives/valida-resposta.directive';
+import { StatusJogoComponent } from '../status-jogo/status-jogo.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { JogoService } from '../services/jogo.service';
+import { Jogo } from '../models/jogo';
+import { Pergunta } from '../models/pergunta';
+import { By } from '@angular/platform-browser';
 
-describe('JogoComponent', () => {
+fdescribe('JogoComponent', () => {
   let component: JogoComponent;
   let fixture: ComponentFixture<JogoComponent>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let jogoService: JogoService;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
+    const rSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
-      declarations: [ JogoComponent ]
-    })
-    .compileComponents();
-  }));
+      imports: [HttpClientTestingModule],
+      declarations: [
+        JogoComponent,
+        StatusJogoComponent,
+        ValidaRespostaDirective
+      ],
+      providers: [
+        { provide: Router, useValue: rSpy }
+      ]
+    });
+
+    routerSpy = TestBed.get(Router);
+    jogoService = TestBed.get(JogoService);
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(JogoComponent);
@@ -19,7 +41,48 @@ describe('JogoComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('#inicializarJogo deve chamar rota home quando não houver jogo acontecendo', () => {
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
+    expect(component.perguntaAtual).toBeUndefined();
+  });
+
+  it('#inicializarJogo deve definir valor de perguntaAtual home quando houver jogo acontecendo', () => {
+    const jogoStub = new Jogo('NomeJogador');
+    const perguntaAtualStub = new Pergunta();
+    perguntaAtualStub.id = 'id-pergunta';
+    perguntaAtualStub.question = 'fake question';
+
+    spyOn(jogoService, 'getJogo').and.returnValue(jogoStub);
+    spyOn(jogoService, 'getPerguntaAtual').and.returnValue(perguntaAtualStub);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(By.css('h2'));
+    const h2: HTMLElement = element.nativeElement;
+
+    expect(routerSpy.navigate).toHaveBeenCalledTimes(1);
+    expect(component.perguntaAtual).toBe(perguntaAtualStub);
+    expect(h2.textContent).toContain(perguntaAtualStub.question);
+  });
+
+  it('#inicializarJogo deve definir valor de perguntaAtual home quando houver jogo acontecendo', () => {
+    const jogoStub = new Jogo('NomeJogador');
+    const perguntaAtualStub = new Pergunta();
+    perguntaAtualStub.id = 'id-pergunta';
+    perguntaAtualStub.question = 'fake question';
+
+    spyOn(jogoService, 'getJogo').and.returnValue(jogoStub);
+    spyOn(jogoService, 'getPerguntaAtual').and.returnValue(perguntaAtualStub);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(By.css('h2'));
+    const h2: HTMLElement = element.nativeElement;
+
+    expect(routerSpy.navigate).toHaveBeenCalledTimes(1);
+    expect(component.perguntaAtual).toBe(perguntaAtualStub);
+    expect(h2.textContent).toContain(perguntaAtualStub.question);
   });
 });
